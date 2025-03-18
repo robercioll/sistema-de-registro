53ff486052b0e494bb223d88b5f6c4508a59b9de
robercioll <robe.espinoza@gmail.com>
2025-03-18 15:50:29 -0300

    Update app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function addParticipants() {
+    const nameInput = document.getElementById('name');
+    const name = nameInput.value.trim();
+    const error = document.getElementById('error');
+
+    if (!isValidName(name, error)){return;}
+
+    participants.push({ name });
+    nameInput.value = "";
+    error.textContent = "";
+    updateParticipantList();
+}
+
+function isValidName(name, error) {
+    if (!name) {
+        error.textContent = "Por favor inserte un nombre";
+        return false;
+    }
+
+    if (participants.some(p => p.name === name)) {
+        error.textContent = "El nombre ya está en la lista.";
+        return false;
+    }
+
+    return true;
+}
+
+function checkEnter(event) {
+    if (event.key === "Enter") {
+        addParticipant();
+    }
+}
+
+function updateParticipantList() {
+    const list = document.getElementById('participantList');
+    list.innerHTML = participants.map(p => `<li>${p.name}</li>`).join('');
+}
